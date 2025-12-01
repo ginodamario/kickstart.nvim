020910b5982d23e128e4f1513a13d076a17c44e8
Gino D'Amario <gino@skygauge.co>
2025-11-30 22:04:35 -0500

    rust

--- init.lua
+++ init.lua
@@ -698,7 +698,11 @@ require('lazy').setup({
             },
           },
         },
-        rust_analyzer = {},
+        -- rust_analyzer = {
+        --   check = {
+        --     command = 'clippy',
+        --   },
+        -- },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -158,4 +158,9 @@ return {
       }
     end,
   },
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^7', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
 }
