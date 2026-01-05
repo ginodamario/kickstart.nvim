d270b70aff33c6383217f17c013736ad37df9e17
Gino D'Amario <gino@skygauge.co>
2026-01-04 20:46:37 -0500

    change to 110 column width

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -3,7 +3,7 @@ vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.softtabstop = 4
 vim.o.smartindent = true
-vim.o.colorcolumn = '100'
+vim.o.colorcolumn = '110'
 -- vim.o.autoindent = true
 vim.o.wrap = false
 
