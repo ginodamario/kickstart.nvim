01c153e3dc7e34327e57c647a2b04c54618bc353
Gino D'Amario <gino@skygauge.co>
2026-02-24 20:09:28 -0500

    Swap xx xX

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -44,12 +44,12 @@ return {
     cmd = 'Trouble',
     keys = {
       {
-        '<leader>xx',
+        '<leader>xX',
         '<cmd>Trouble diagnostics toggle<cr>',
         desc = 'Diagnostics (Trouble)',
       },
       {
-        '<leader>xX',
+        '<leader>xx',
         '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
         desc = 'Buffer Diagnostics (Trouble)',
       },
@@ -163,26 +163,26 @@ return {
     version = '^7', -- Recommended
     lazy = false, -- This plugin is already lazy
   },
-  {
-    'hat0uma/csvview.nvim',
-    ---@module "csvview"
-    ---@type CsvView.Options
-    opts = {
-      parser = { comments = { '#', '//' } },
-      keymaps = {
-        -- Text objects for selecting fields
-        textobject_field_inner = { 'if', mode = { 'o', 'x' } },
-        textobject_field_outer = { 'af', mode = { 'o', 'x' } },
-        -- Excel-like navigation:
-        -- Use <Tab> and <S-Tab> to move horizontally between fields.
-        -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
-        -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
-        jump_next_field_end = { '<Tab>', mode = { 'n', 'v' } },
-        jump_prev_field_end = { '<S-Tab>', mode = { 'n', 'v' } },
-        jump_next_row = { '<Enter>', mode = { 'n', 'v' } },
-        jump_prev_row = { '<S-Enter>', mode = { 'n', 'v' } },
-      },
-    },
-    cmd = { 'CsvViewEnable', 'CsvViewDisable', 'CsvViewToggle' },
-  },
+  -- {
+  --   'hat0uma/csvview.nvim',
+  --   ---@module "csvview"
+  --   ---@type CsvView.Options
+  --   opts = {
+  --     parser = { comments = { '#', '//' } },
+  --     keymaps = {
+  --       -- Text objects for selecting fields
+  --       textobject_field_inner = { 'if', mode = { 'o', 'x' } },
+  --       textobject_field_outer = { 'af', mode = { 'o', 'x' } },
+  --       -- Excel-like navigation:
+  --       -- Use <Tab> and <S-Tab> to move horizontally between fields.
+  --       -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
+  --       -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+  --       jump_next_field_end = { '<Tab>', mode = { 'n', 'v' } },
+  --       jump_prev_field_end = { '<S-Tab>', mode = { 'n', 'v' } },
+  --       jump_next_row = { '<Enter>', mode = { 'n', 'v' } },
+  --       jump_prev_row = { '<S-Enter>', mode = { 'n', 'v' } },
+  --     },
+  --   },
+  --   cmd = { 'CsvViewEnable', 'CsvViewDisable', 'CsvViewToggle' },
+  -- },
 }
